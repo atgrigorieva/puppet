b37a7fe35d735bf45916bd92131942d50653f63d
atgrigorieva <flora992@yandex.ru>
2016-06-22 00:50:33 +0400

    Update hellojava.pp

--- hellojava.pp
+++ hellojava.pp
@@ -1,13 +1,7 @@
-node default {
-  java::oracle { 'jdk8' :
-  ensure  => 'present',
-  version => '8',
-  java_se => 'jdk',
-  }
-  class { 'oracle_java':
-   version => '8u45',
-   type    => 'jdk'
-  }
-  class {'java8': }
-  #class {'scala': }
-}
+file { '/tmp/helloworld':
+  ensure => present,
+  content => 'Hello, world!\n',
+  mode => 0644,
+  owner => 'ec2-user',
+  group => 'root'
+} 
